b2fb28e0ff0fa1274da951f3124a5f2cbc86efa7
ashif-2005 <mhmdashif05@gmail.com>
2024-08-11 18:53:09 +0530

    modified backend

--- FrontEnd/leave-management/src/pages/Employee/EmployeeHome.jsx
+++ FrontEnd/leave-management/src/pages/Employee/EmployeeHome.jsx
@@ -10,21 +10,17 @@ function EmployeeHomePage() {
   const { username } = useParams();
   console.log(username);
 
-  useEffect(()=>{
-    getData();
-  },[])
+  // useEffect(()=>{
+  //   getData();
+  // },[])
 
   return (
     <>
     <div className={`w-screen flex relative `}>
       <Nav/> 
-<<<<<<< HEAD
    
       <ViewSide username = {username}/> 
 
-=======
-      <ViewSide/> 
->>>>>>> 6b2e1dd700d576153a6bd3412ed8ab7991367c82
    </div>
     </>
     
